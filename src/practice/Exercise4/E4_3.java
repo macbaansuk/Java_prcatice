2b0ae5122e6c12202fd9a981575180335c5e4d3e
macbaansuk <macbaansuk@gmail.com>
2023-06-02 20:47:28 +0900

    [230602] 연습문제 4_3

--- src/practice/Exercise4/E4_3.java
+++ src/practice/Exercise4/E4_3.java
@@ -3,8 +3,11 @@ package practice.Exercise4;
 //1+(1+2)+(1+2+3)+(1+2+3+4)+...+(1+2+3+...+10)의 결과를 계산하시오
  class E4_3 {
     public static void main(String[] args) {
+        int sum = 0;
         for(int i = 0; i<=10; i++){
-//            int
+            //1씩 증가하면서 계속 더하기
+             sum += i;
         }
+        System.out.println("sum = " + sum);
     }
 }
