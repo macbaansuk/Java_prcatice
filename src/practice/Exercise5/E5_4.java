b5fc30d6bc1143bec1434d8cb7dc5f918d8b5082
macbaansuk <macbaansuk@gmail.com>
2023-06-23 22:16:52 +0900

    [230623] 연습문제 풀기

--- src/practice/Exercise5/E5_4.java
+++ src/practice/Exercise5/E5_4.java
@@ -9,15 +9,18 @@ class E5_4 {
                 {20,20,20,20,20},
                 {30,30,30,30,30}
         };
-        int total = 0;
-        float average = 0;
+        int total = 0; //총합
+        float average = 0; //평균
+
+        //평균 = 총합 / 배열의 길이
 
         for(int i =0; i < arr.length; i++){
             for(int j=0; j<arr[i].length; j++){
                 total += arr[i][j];
-                average = (float)total / arr[i].length;
             }
+//                average = (float)total / arr[i].length;  //이렇게 작성하면 65.0이나옴
         }
+        average = total /(float)(arr.length * arr[0].length);
         System.out.println("total="+total);
         System.out.println("average="+average);
 
